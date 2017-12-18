e54fa6c0583486fd655098f4ecc8b452c384023a
cheng <574637816@qq.com>
2017-12-18 21:53:17 +0900

    all

--- Allien_invasion/alien_invasion.py
+++ Allien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 import game_functions as gf
 from settings import Settings
 from ship import Ship
@@ -12,15 +13,22 @@ def run_game():
 	pygame.display.set_caption("Alien Invasion")
 
 	#创建一艘飞船的实例，导入Ship类
-	ship = Ship(screen)
-
-	## Set the background color
+	ship = Ship(ai_settings,screen)
+	#创建一个用于存储子弹的编组
+	bullets = Group()
+	#创建一个用于存储外星人的编组
+	aliens = Group()
+	#创建外星人群
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+	# Set the background color
 	bg_color = (230,230,230)
 
 	# Start the main loop for the game
 	while True:
-		gf.check_events(ship)
+		gf.check_events(ai_settings,screen,ship,bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		gf.update_bullets(aliens,bullets)
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- Allien_invasion/chapter_12/bullet.py
+++ Allien_invasion/chapter_12/bullet.py
@@ -10,8 +10,7 @@ class Bullet(Sprite):
         self.screen = screen
 
         # Create bullet rect at (0, 0), then set correct position.
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
-            ai_settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
         
