174af3c26f8aac1081d93c92172f8fd96d30a44e
cheng <574637816@qq.com>
2018-01-11 21:37:47 +0900

    Give 4 chances

--- Python_quicklearning/guess_number.py
+++ Python_quicklearning/guess_number.py
@@ -3,15 +3,17 @@ import random
 sum=0
 number = random.randint(1,20)
 print("I am think of a number between 1 and 20")
-
-while True：
+print("You have 4 chances!")
+for i in range(5):
 
 	guess_n = int(input("Take a guess\n"))
 	sum += 1
-	if guess_n < number：
+	if i == 4:
+		print("Game over!")
+	elif guess_n < number:
 		print ("your guess is too low" )
 	elif guess_n > number:
 		print ("your guess is too high")
 	elif guess_n == number:
-		print ("Good job! your guessed my number is " + sum + " guesses")
+		print ("Good job! your guessed my number is " + str(sum) + " guesses")
 		break
